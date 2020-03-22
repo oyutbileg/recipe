718a02ff923537fa23bf3ad45712ee0ddc166c5d
oyut <oyutbileg@gmail.com>
2020-03-22 19:52:04 +0800

    search controller hiiv

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,30 @@
 require("@babel/polyfill");
 import Search from "./model/search";
 
-let search = new Search("pasta");
+// Web APP iin tuluw
+//Hailtiin query, ur dun
+//Tuhain uzuulj baigaa jor
+// Like iin joruud
+//Zahialj baigaa joriin nairlaguud
+const state = {};
 
-search.doSearch().then(r => console.log(r));
+const controlSearch = async () => {
+  // 1)Web ees hailtiin tulhuur ugiig gargaj avna
+  const query = "pizza";
+
+  if (query) {
+    // 2)Shineer hailtiin object iig uusgej ugnu
+    state.search = new Search(query);
+    // 3)hailt hiihed zoriulj UI beltgen
+
+    // 4)Hailtiig guitsetgen
+    await state.search.doSearch();
+    //5) hailtiin ur dung delgetsend uzuulne
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
